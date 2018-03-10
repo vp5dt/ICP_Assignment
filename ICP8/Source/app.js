317d1e9d75119555cb408d1656b1902ab6d4111e
bhavesh <vp5dt@github.com>
2018-03-09 20:51:56 -0600

    Code and screen shots
    Code and screen shots

---
+++ ICP8/Source/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [])
+
+
+    .controller('View1Ctrl', function ($scope, $http) {
+        $scope.venueList = new Array();
+        $scope.mostRecentReview;
+        $scope.getVenues = function () {
+            var placeEntered = document.getElementById("txt_placeName").value;
+
+            if (placeEntered != null && placeEntered != "") {
+                document.getElementById('div_ReviewList').style.display = 'none';
+                //This is the API that gives the list of venues based on the place and search query.
+                var handler = $http.get("https://sandbox-healthservice.priaid.ch/issues/"+placeEntered+"/info?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImJoYXZlc2hyZWRkeTk5OUBnbWFpbC5jb20iLCJyb2xlIjoiVXNlciIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL3NpZCI6IjI5MTAiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3ZlcnNpb24iOiIyMDAiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL2xpbWl0IjoiOTk5OTk5OTk5IiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9tZW1iZXJzaGlwIjoiUHJlbWl1bSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbGFuZ3VhZ2UiOiJlbi1nYiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvZXhwaXJhdGlvbiI6IjIwOTktMTItMzEiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL21lbWJlcnNoaXBzdGFydCI6IjIwMTgtMDItMjYiLCJpc3MiOiJodHRwczovL3NhbmRib3gtYXV0aHNlcnZpY2UucHJpYWlkLmNoIiwiYXVkIjoiaHR0cHM6Ly9oZWFsdGhzZXJ2aWNlLnByaWFpZC5jaCIsImV4cCI6MTUyMDYyNjc4MywibmJmIjoxNTIwNjE5NTgzfQ.FxtPiLdOEtlSgKrd1LLLArJ9SrcKAxinlQ3PWOfyUCM&language=en-gb&format=json");
+                handler.success(function (data) {
+
+
+                            $scope.venueList[0] = {
+                                "name": data.Description
+                            };
+
+
+
+                })
+                handler.error(function (data) {
+                    alert("There was some error processing your request. Please try after some time.");
+                });
+            }
+        }
+
+
+    });
